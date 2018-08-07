8fab7808263de114e513e081e63c4483cc1853c2
Daniel Negassi <dyn29@hotmail.com>
2018-08-07 10:25:55 -0500

    created Nav bar

--- react-frontend/src/Nav/index.js
+++ react-frontend/src/Nav/index.js
@@ -1,1 +1,15 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const Nav = () => {
+  return (
+      <nav>
+        <ul>
+          <li><Link to ='/'>Login</Link></li>
+          <li><Link to='/profile'>My Profile</Link></li>
+        </ul>
+      </nav>
+    )
+}
+
+export default Nav;
