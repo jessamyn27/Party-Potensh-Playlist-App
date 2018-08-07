0d1ab250a346b8d832033065fc4b6c098e7d8044
Timothy Mickle <timothyrmickle@gmail.com>
2018-08-07 11:27:24 -0500

    added main Spotify component + playlist finder + song finder

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Login from './Login';
-import Profile from './Profile';
+// import Profile from './Profile';
 
 
 import './App.css';
-import Login from './Login';
 import About from './About';
 import Edit from './Edit';
 import Parties from './Parties';
 import CreateParty from './CreateParty';
 import Register from './Register';
-import Profile from './';
+
+import Spotify from './Spotify'
 
 
 class App extends Component {
   render() {
     return (
+      <div>
       <main>
       <Switch>
         <Route exact path='/' component={Login}/>
-        <Route exact path='/profile' component={Profile}/>
+        {/* <Route exact path='/profile' component={Profile}/> */}
       </Switch>
     </main>
       <div className="App">
@@ -31,9 +32,10 @@ class App extends Component {
         {/* <CreateParty /> */}
         <Register />
         {/* <Profile /> */}
+        <Spotify/>
 
       </div>
-
+      </div>
     );
   }
 }
--- react-frontend/src/Spotify/index.js
+++ react-frontend/src/Spotify/index.js
@@ -1,1 +1,77 @@
 import React, {Component} from 'react';
+import queryString from 'query-string';
+import SpotifyPlaylistSearch from '../SpotifyPlaylistSearch';
+import SearchForSong from '../SongSearch';
+
+class Spotify extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {},
+      filterString: '',
+      playlists: [],
+      spotifyUserID: '',
+      spotifyName: '',
+      access_token: '',
+    }
+  }
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => {
+      console.log(data, 'user data ME');
+      return this.setState({
+        spotifyUserID: data.id,
+        spotifyName: data.displayName,
+        access_token: accessToken,
+    }) })
+
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken, "Accept": "application/json","Content-Type": "application/json"
+    }
+    }).then(response =>  response.json())
+    .then(data => this.setState({
+      playlists: data.items.map(item => {
+        console.log(data, 'data in playlist')
+        return {
+          name: item.name,
+          imageUrl: item.images.map((item)=>{
+            return item;
+          }), 
+          songs: []
+        }
+    })
+    })
+  )
+
+  } 
+  render() {
+
+    // *** THIS IS PRACTIVE API MANGLING 
+    // console.log(this.state, 'this is the state')
+    // const playlists = this.state.playlists.map((item)=> {
+    //   console.log(item.imageUrl[0], 'this is an item')
+    //   return item.imageUrl.map((item) => {
+    //    console.log(item.url, 'imageURL map')
+    //    if (item.url === undefined) {} else {
+    //    return <img src={item.url}/> }
+    //  })
+    // })
+    // console.log(playlists);
+    console.log(this.state, 'this state')
+    return (
+    <div>
+    <h1>Spotify Main Container</h1>
+      <SpotifyPlaylistSearch access_token={this.state.access_token}/>
+      <SearchForSong access_token={this.state.access_token}/>
+    </div>
+    )
+  }
+}
+
+export default Spotify;
